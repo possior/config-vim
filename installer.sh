c004cf5cb57be2ae15d20c8bc30513e58eaa5731
possior <153281819+possior@users.noreply.github.com>
2026-08-08 18:35:10 +0900

    20260808 | Modified /installer.sh

--- installer.sh
+++ installer.sh
@@ -1,40 +1,40 @@
 function overwriter() {
   if
-    [[ ${1##*/} == .vimrc ]]
+    [[ "${1##*/}" =~ ^\. ]]
   then
-    curl -so ${HOME}/${1##*/} ${1}
+    curl -fsSLo "$HOME/${1##*/}" "$1"
   else
-    curl -so ${HOME}/.config/vim/${1##*/} ${1}
+    curl -fsSLo "$HOME/.config/vim/${1##*/}" "$1"
   fi
 }
 
 function preserver() {
   if
-    [[ ${1##*/} == .vimrc ]]
+    [[ "${1##*/}" =~ ^\. ]]
   then
     if
-      [[ ! -f ${HOME}/${1##*/} ]]
+      [[ ! -f "$HOME/${1##*/}" ]]
     then
-      curl -so ${HOME}/${1##*/} ${1}
+      curl -fsSLo "$HOME/${1##*/}" "$1"
     fi
   else
     if
-      [[ ! -f ${HOME}/${1##*/} ]]
+      [[ ! -f "$HOME/${1##*/}" ]]
     then
-      curl -so ${HOME}/.config/vim/${1##*/} ${1}
+      curl -fsSLo "$HOME/.config/vim/${1##*/}" "$1"
     fi
   fi
 }
 
-echo ".. initiated the process"
+echo ":: initiated the process"
 
 while
-  [[ $# -gt 0 ]]
+  [[ "$#" -gt "0" ]]
 do
   case "$1" in
     "-o" | "--overwrite")
       if
-        [[ -z "${behavior}" ]]
+        [[ -z "$behavior" ]]
       then
         behavior="overwrite"
         shift 1
@@ -45,7 +45,7 @@ do
       ;;
     "-p" | "--preserve")
       if
-        [[ -z "${behavior}" ]]
+        [[ -z "$behavior" ]]
       then
         behavior="preserve"
         shift 1
@@ -57,26 +57,28 @@ do
   esac
 done
 
-echo ".. parsed arguments"
+echo ":: parsed arguments"
 
 if
-  [[ ! -d ${HOME}/.config/vim ]]
+  [[ ! -d "$HOME/.config/vim" ]]
 then
-  mkdir -p ${HOME}/.config/vim
-  echo ".. created the configuration directory"
+  mkdir -p "$HOME/.config/vim"
+  echo ":: created the configuration directory"
 fi
 
 case "${behavior:-overwrite}" in
   "overwrite")
-    overwriter https://raw.githubusercontent.com/possior/config-vim/default/src/.vimrc
-    echo ".. downloaded configuration files (overwrite)"
+    overwriter "https://raw.githubusercontent.com/possior/config-vim/default/src/.vimrc"
+    echo ":: downloaded configuration files (overwrite)"
     ;;
   "preserve")
-    preserver https://raw.githubusercontent.com/possior/config-vim/default/src/.vimrc
-    echo ".. downloaded configuration files (preserve)"
+    preserver "https://raw.githubusercontent.com/possior/config-vim/default/src/.vimrc"
+    echo ":: downloaded configuration files (preserve)"
     ;;
   *)
     echo "!! failed downloadation due to unknown internal variable value"
     exit
     ;;
 esac
+
+echo ":: completed downloading configuration and documentation"
